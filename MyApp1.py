a86dcac5217143d4cbdade83a1c605b353966ee7
kittichokv18 <kittichokv18@gmail.com>
2024-03-28 11:09:32 +0700

    Update MyApp1.py

--- MyApp1.py
+++ MyApp1.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("website Developing using python")
 st.header("website Developing using python")
-st.subheader("kittichok khumkhrong")
+st.subheader("9arm ใจเกเล")
 st.image('popass.jpg')
