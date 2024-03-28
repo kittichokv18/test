a210137656f15f361c299ce6dc8b45431c77b86e
kittichokv18 <kittichokv18@gmail.com>
2024-03-28 11:59:31 +0700

    Update MyApp1.py

--- MyApp1.py
+++ MyApp1.py
@@ -26,48 +26,29 @@ cl3.write(dt['petal.length'].sum())
 cl4.write("petal.width")
 cl4.write(dt['petal.width'].sum())
 
-cols=['sepal.length','sepal.width','petal.length']
-dx=dt[cols]
-st.bar_chart(dx,x='sepal.length',y='sepal.width',color='petal.length')
+cols = ['sepal.length', 'sepal.width', 'petal.length']
+dx = dt[cols]
+
+st.subheader("กราฟแท่ง")
+st.bar_chart(dx)
 
 st.write("ค่าเฉลี่ย")
 cl1,cl2,cl3,cl4=st.columns(4)
-cl1.write("sepal.length")
 cl1.write(dt['sepal.length'].mean())
-
-cl2.write("sepal.width")
 cl2.write(dt['sepal.width'].mean())
-
-cl3.write("petal.length")
 cl3.write(dt['petal.length'].mean())
-
-cl4.write("petal.width")
 cl4.write(dt['petal.width'].mean())
 
 st.write("ค่ามากที่สุด")
 cl1,cl2,cl3,cl4=st.columns(4)
-cl1.write("sepal.length")
 cl1.write(dt['sepal.length'].max())
-
-cl2.write("sepal.width")
 cl2.write(dt['sepal.width'].max())
-
-cl3.write("petal.length")
 cl3.write(dt['petal.length'].max())
-
-cl4.write("petal.width")
 cl4.write(dt['petal.width'].max())
 
 st.write("ค่าน้อยที่สุด")
 cl1,cl2,cl3,cl4=st.columns(4)
-cl1.write("sepal.length")
 cl1.write(dt['sepal.length'].min())
-
-cl2.write("sepal.width")
 cl2.write(dt['sepal.width'].min())
-
-cl3.write("petal.length")
 cl3.write(dt['petal.length'].min())
-
-cl4.write("petal.width")
 cl4.write(dt['petal.width'].min())
