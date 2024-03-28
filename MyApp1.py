a32be8775b111ac7ed0ca9e74be36a8d4aac7fff
kittichokv18 <kittichokv18@gmail.com>
2024-03-28 11:26:29 +0700

    Update MyApp1.py

--- MyApp1.py
+++ MyApp1.py
@@ -1,6 +1,10 @@
 import streamlit as st
+import pandas as pd
 
 st.title("website Developing using python")
 st.header("website Developing using python")
 st.subheader("9arm ใจเกเล")
 st.image('img/dance.gif')
+
+dt=pd.read_csv('data/iris.csv')
+st.write(dt.head(10))
