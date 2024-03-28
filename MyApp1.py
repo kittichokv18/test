835d5e794cb3ed7521d4cd9f762c4f0ac236af6a
kittichokv18 <kittichokv18@gmail.com>
2024-03-28 11:39:16 +0700

    Update MyApp1.py

--- MyApp1.py
+++ MyApp1.py
@@ -14,8 +14,14 @@ st.write(dt.head(10))
 st.subheader("สถิติข้อมูลดอกไม้ Iris")
 st.write("ผลรวม")
 cl1,cl2,cl3,cl4=st.columns(4)
-cl1.write(dt['sepal.length'].sum()) 
-cl2.write(dt['sepal.width'].sum()) 
-cl3.write(dt['petal.length'].sum()) 
-cl4.write(dt['petal.width'].sum()) 
+cl1.write("ผลรวม")
+cl1.write(dt['sepal.length'].sum())
 
+cl2.write("ผลรวม")
+cl2.write(dt['sepal.width'].sum())
+
+cl3.write("ผลรวม")
+cl3.write(dt['petal.length'].sum())
+
+cl4.write("ผลรวม")
+cl4.write(dt['petal.width'].sum())
